87eb8d733e6cffdd1aa5a503393ce2f54f2a2848
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-02 17:07:36 +0400

    question counters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,26 +3,19 @@ module ApplicationHelper
     user.avatar_url.presence || asset_pack_path('media/images/avatar.jpg')
   end
 
-  def question_counter(questions)
-    question_counter = questions.count
-    "У Вас #{question_counter} #{inclination(question_counter)}"
-  end
-
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
 
-  private
-
   def inclination(number)
-    return  'вопросов' if (11..14).include?(number % 100)
+    return  'ов' if (11..14).include?(number % 100)
 
     last_digit = number % 10
 
-    return  'вопрос' if last_digit == 1
+    return  '' if last_digit == 1
 
-    return  'вопроса' if (2..4).include?(last_digit)
+    return  'а' if (2..4).include?(last_digit)
 
-    'вопросов'
+    'ов'
   end
 end
