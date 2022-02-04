895066f402a6daee2647a3df7229d148a7ad2527
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-04 14:10:44 +0400

    user color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_101813) do
+ActiveRecord::Schema.define(version: 2022_02_04_071901) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_01_28_101813) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "color"
   end
 
   add_foreign_key "questions", "users"
